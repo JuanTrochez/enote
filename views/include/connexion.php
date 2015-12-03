adff2aea7157bb0dad7bb1351ee194b574e96e54
Hcroiss <croissant.herve@gmail.com>
2015-12-03 12:21:30 +0100

    CSS

--- views/include/connexion.php
+++ views/include/connexion.php
@@ -3,11 +3,11 @@
 
 <form class="form" action="" method="POST">
     
-    <input class="form-control" name="login" type="text" placeholder="Login">
+    <input class="form-control champ-form" name="login" type="text" placeholder="Login">
     <br />
-    <input class="form-control" name="pwd" type="password" placeholder="Password">
+    <input class="form-control champ-form" name="pwd" type="password" placeholder="Password">
     <br />
-    <input class="submit" name="submit" type="submit" value="Connect">
+    <input class="btn btn-primary" name="submit" type="submit" value="Connect">
 </form>
 
 
