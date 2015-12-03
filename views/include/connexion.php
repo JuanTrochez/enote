e7f3e55a57d4a2a76aa3297b7a9c9958a8ea064f
Hcroiss <croissant.herve@gmail.com>
2015-12-03 11:38:25 +0100

    CSS

--- views/include/connexion.php
+++ views/include/connexion.php
@@ -1,10 +1,11 @@
 
 <div>Bienvenue sur notre....</div>
 
-<form action="" method="POST">
-    <input class="coco" name="login" type="text">
+<form class="form" action="" method="POST">
+    
+    <input class="form-control" name="login" type="text" placeholder="Login">
     <br />
-    <input class="pwd" name="pwd" type="password">
+    <input class="form-control" name="pwd" type="password" placeholder="Password">
     <br />
     <input class="submit" name="submit" type="submit" value="Connect">
 </form>
