d8a63dca50ee555c7a81f2b8119c8ecdcacd9ef3
afonclaud <lebrazideck@hotmail.fr>
2015-12-03 12:27:17 +0100

    Changement dans le header

--- views/global/header.php
+++ views/global/header.php
@@ -15,39 +15,7 @@ and open the template in the editor.
     <body>
         <header>
             <html lang="en">
-              <head>
-                <meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-                <meta name="description" content="">
-                <meta name="author" content="">
-                <link rel="icon" href="../../favicon.ico">
-
-                <title>Fixed Top Navbar Example for Bootstrap</title>
-
-                <!-- Bootstrap core CSS -->
-                <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-                <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-                <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-                <!-- Custom styles for this template -->
-                <link href="navbar-fixed-top.css" rel="stylesheet">
-
-                <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-                <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-                <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-                <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-                <!--[if lt IE 9]>
-                  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-                  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-                <![endif]-->
-              </head>
-
               <body>
-
                 <!-- Fixed navbar -->
                 <nav class="navbar navbar-default navbar-fixed-top">
                   <div class="container">
@@ -86,22 +54,6 @@ and open the template in the editor.
                     </div><!--/.nav-collapse -->
                   </div>
                 </nav>
-
-                <div class="container">
-
-                  <!-- Main component for a primary marketing message or call to action -->
-                  <div class="jumbotron">
-                    <h1>Navbar example</h1>
-                    <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-                    <p>To see the difference between static and fixed top navbars, just scroll.</p>
-                    <p>
-                      <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-                    </p>
-                  </div>
-
-                </div> <!-- /container -->
-
-
                 <!-- Bootstrap core JavaScript
                 ================================================== -->
                 <!-- Placed at the end of the document so the pages load faster -->
