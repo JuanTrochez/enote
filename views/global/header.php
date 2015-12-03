32d5ed4c288aac3477249571a8fc56a6db9334fa
Juan <juan.trochez@hotmail.fr>
2015-12-03 12:41:56 +0100

    header bootstrap

--- views/global/header.php
+++ views/global/header.php
@@ -14,8 +14,6 @@ and open the template in the editor.
     </head>
     <body>
         <header>
-            <html lang="en">
-              <body>
                 <!-- Fixed navbar -->
                 <nav class="navbar navbar-default navbar-fixed-top">
                   <div class="container">
@@ -53,18 +51,7 @@ and open the template in the editor.
                       </ul>
                     </div><!--/.nav-collapse -->
                   </div>
-                </nav>
-                <!-- Bootstrap core JavaScript
-                ================================================== -->
-                <!-- Placed at the end of the document so the pages load faster -->
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-                <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-                <script src="../../dist/js/bootstrap.min.js"></script>
-                <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-                <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-              </body>
-            </html>
-
+                </nav>               
         </header>
         
         <section class ="section">
