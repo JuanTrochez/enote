f3aa196124b31a69ec1bc2768523fdb5b11969ee
Hcroiss <croissant.herve@gmail.com>
2015-12-03 14:20:54 +0100

    Trad FR + checkbox

--- views/include/connexion.php
+++ views/include/connexion.php
@@ -1,11 +1,15 @@
 
-<div>Bienvenue sur notre....</div>
+<div>Bienvenue</div>
 
 <form class="form" action="" method="POST">
     
-    <input class="form-control champ-form" name="login" type="text" placeholder="Login">
+    <input class="form-control champ-form" name="login" type="text" placeholder="Pseudo">
     <br />
-    <input class="form-control champ-form" name="pwd" type="password" placeholder="Password">
+    <input class="form-control champ-form" name="pwd" type="password" placeholder="Mot de passe">
+    <br />
+    <div class="checkbox">
+        <label><input type="checkbox"> Rester connecté </label>
+    </div>
     <br />
     <input class="btn btn-primary" name="submit" type="submit" value="Connect">
 </form>
