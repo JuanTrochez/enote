a70b32aaf0f2fac45e9e1a1b7615bee628d4b9c5
Hcroiss <croissant.herve@gmail.com>
2015-12-03 15:01:14 +0100

    connection_2

--- controller/accueilController.php
+++ controller/accueilController.php
@@ -1,4 +1,7 @@
 <?php
+
+use User;
+
 if ($_SESSION['user'])
 {
     include_once "/views/include/accueil.php";
@@ -7,9 +10,14 @@ elseif (isset($_COOKIE['login']) && !empty($_COOKIE['login']) &&
         isset($_COOKIE['password']) && !empty($_COOKIE['password']))
     {
         cookie($_COOKIE['login'],$_COOKIE['password'],true);
+        $user = new User();
+        $user->setLogin($_COOKIE['login']);
+        $user->setPassword($_COOKIE['password']);
+        $user->connect($bdd,true);
         include_once "/views/include/accueil.php";
     }
 else 
 {
     include_once "/views/include/connexion.php";
 }
+
