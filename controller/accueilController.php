d2fa5d8050cd42e4d79016153a17cb2a2d7d4207
Hcroiss <croissant.herve@gmail.com>
2015-12-03 14:59:46 +0100

    connection

--- controller/accueilController.php
+++ controller/accueilController.php
@@ -3,6 +3,12 @@ if ($_SESSION['user'])
 {
     include_once "/views/include/accueil.php";
 }
+elseif (isset($_COOKIE['login']) && !empty($_COOKIE['login']) &&
+        isset($_COOKIE['password']) && !empty($_COOKIE['password']))
+    {
+        cookie($_COOKIE['login'],$_COOKIE['password'],true);
+        include_once "/views/include/accueil.php";
+    }
 else 
 {
     include_once "/views/include/connexion.php";
