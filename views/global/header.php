fbcee3f328aee12e43a358eefde51f965123ec5a
Hcroiss <croissant.herve@gmail.com>
2015-12-04 16:20:38 +0100

    sidebar

--- views/global/header.php
+++ views/global/header.php
@@ -16,49 +16,44 @@ and open the template in the editor.
     </head>
     <body>
         <header>
-                <!-- Fixed navbar -->
-                <nav class="navbar navbar-default navbar-fixed-top">
-                  <div class="container">
-                    <div class="navbar-header">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                      <a class="navbar-brand" href="<?php echo $basePath; ?>">Enote</a>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                      <ul class="nav navbar-nav">
-                        <li class="active"><a href="<?php echo $basePath; ?>">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                          <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                          </ul>
-                        </li>
-                      </ul>
-                        <?php
+           <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="<?php echo $basePath; ?>">Enote</a>
+                </li>
+                <li>
+                    <a href="<?php echo $basePath; ?>">Home</a>
+                </li>
+                <li>
+                    <a href="#">Shortcuts</a>
+                </li>
+                <li>
+                    <a href="#">Overview</a>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <?php
                         if($secu->logged())
                         {
                             ?>
-                           <ul class="nav navbar-nav navbar-right">
-                            <li class="active"><a href="?page=deconnexion">Déconnexion<span class="sr-only">(current)</span></a></li>
-                           </ul> 
+                           
+                <li class="deco">
+                    <a href="?page=deconnexion">Déconnexion<span class="sr-only">(current)</span></a>
+                </li>
                         <?php
                         }
                         ?>
-                    </div><!--/.nav-collapse -->
-                  </div>
-                </nav>               
+                </li>
+            </ul>
+        </div>
         </header>
         
         <section class ="section">
